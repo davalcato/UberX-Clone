866b2ea4c8c0b3d22f4729c29d13e5dd53c8533b
davalcato <davalcato@yahoo.com>
2020-09-05 14:07:07 -0700

    added the @State var map = MKMapView() along with the @Binding var map : MKMapView to give it the ability to search MKMapView

--- UberX/ContentView.swift
+++ UberX/ContentView.swift
@@ -11,7 +11,8 @@ import CoreLocation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+       
+        Home()
     }
 }
 
@@ -20,3 +21,62 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View {
+    
+    @State var map = MKMapView()
+    @State var manager = CLLocationManager()
+    
+    var body: some View{
+        
+        ZStack{
+            
+            VStack(spacing: 0){
+                
+                HStack{
+                    
+                    Text("Pick a Location")
+                        .font(.title)
+                    
+                    Spacer()
+                }
+                .padding()
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                .background(Color.white)
+                
+                MapView(map: self.$map)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MapView : UIViewRepresentable {
+    @Binding var map : MKMapView
+
+    func makeUIView(context: Context) -> MKMapView {
+        
+        return map
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
