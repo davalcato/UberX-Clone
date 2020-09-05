d5c521dd87fbd5b2537dddfdabbe19b88b61e169
davalcato <davalcato@yahoo.com>
2020-09-05 13:41:47 -0700

    first commit

---
+++ UberX/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  UberX
+//
+//  Created by Daval Cato on 9/5/20.
+//
+
+import SwiftUI
+import MapKit
+import CoreLocation
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UberX/UberXApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberXApp.swift
+//  UberX
+//
+//  Created by Daval Cato on 9/5/20.
+//
+
+import SwiftUI
+
+@main
+struct UberXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
